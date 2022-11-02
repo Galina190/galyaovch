e5e4e2c7e3e88f512de9d04ccc95dd341279e719
galyaovch <Galya.Ovch@list.ru>
2022-11-02 07:52:03 +0000

    38.1

---
+++ terms/38.1.php
@@ -0,0 +1,16 @@
+<?php
+	$min = 10;
+	
+	if ($min >= 0 and $min <= 19) {
+		echo '1 четверть';
+	}
+	
+	if ($min >= 20 and $min <= 39) {
+		echo '2 четверть';
+	}
+	
+	if ($min >= 40 and $min <= 59) {
+		echo '3 четверть';
+	}
+	
+?>
