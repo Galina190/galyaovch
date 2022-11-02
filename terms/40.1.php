069d585e25b2e443f98dab76ae51dd150db3b561
galyaovch <Galya.Ovch@list.ru>
2022-11-02 10:30:31 +0000

    40.1

---
+++ terms/40.1.php
@@ -0,0 +1,18 @@
+<?php 
+$num = '4';
+
+switch ($num) {
+    case '1':
+        echo 'зима';
+    break;
+    case '2':
+        echo 'весна';
+    break;
+    case '3':
+        echo 'лето';
+    break;
+    case '4':
+        echo 'осень';
+    break;
+}
+?>
