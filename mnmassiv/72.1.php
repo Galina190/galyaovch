63993e0f1736a090336114cac43d165603aa01f3
galyaovch <Galya.Ovch@list.ru>
2022-11-30 08:40:39 +0000

    72.1

---
+++ mnmassiv/72.1.php
@@ -0,0 +1,22 @@
+<?php
+	$products = [
+		[
+			'name'   => 'мясо',
+			'price'  => 100,
+			'amount' => 5,
+		],
+		[
+			'name'   => 'овощи',
+			'price'  => 200,
+			'amount' => 6,
+		],
+		[
+			'name'   => 'фрукты',
+			'price'  => 300,
+			'amount' => 7,
+		],
+	];
+    foreach ($products as $products) {
+		echo "Наименование продукта:" .$products['name'] . ', цена за ед: ' . $products['price'] . 'руб, в наличии: ' . $products ['amount']. '<br>' ;
+	}
+?>
